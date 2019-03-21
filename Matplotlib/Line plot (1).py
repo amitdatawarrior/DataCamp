3e1222415d6f2ca81ed8d2fbbae25cb7332985d0
KUMAR-P Amit <amit.kumar-p@rntbci.com>
2019-03-21 17:03:00 +0530

    LinePlot Correction 1

--- Matplotlib/Line plot (1).py
+++ Matplotlib/Line plot (1).py
@@ -2,8 +2,12 @@
 #The most basic plot is the line plot. A general recipe is given here.
 #Print the last item from year and pop
 # Import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
+# Print the last item from year and pop
+print(year[-1])
+print(pop[-1])
 
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Make a line plot: year on the x-axis, pop on the y-axis
 plt.plot(year, pop)
