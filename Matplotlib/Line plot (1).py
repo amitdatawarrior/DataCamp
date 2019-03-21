9075c3bc10ae130c219535856e739b4699c6e1f7
z021753 <amit.kumar-p@rntbci.com>
2019-03-21 15:33:16 +0530

    This is the commit for LinePlot

---
+++ Matplotlib/Line plot (1).py
@@ -0,0 +1,12 @@
+#With matplotlib, you can create a bunch of different plots in Python.
+#The most basic plot is the line plot. A general recipe is given here.
+#Print the last item from year and pop
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
